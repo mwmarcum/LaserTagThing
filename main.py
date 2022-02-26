8d58c3eb2d51d7e0a93a5ee7311d5b9098db925d
nesquack <karsen.beck@gmail.com>
2022-02-25 20:37:01 -0600

    connect splash screen to main window

--- main.py
+++ main.py
@@ -2,40 +2,83 @@ import sys
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from playerEntryScreen import Ui_MainWindow
 
-class Window(QWidget):
+# pyqt/python is stupid and will immediately gc all windows
+# so hold a reference to all windows so it knows not to destroy them
+main_window = None
+
+class SplashWindow(QWidget):
+    closed = pyqtSignal()
+    
     def __init__(self, *args, **kwargs):
         QWidget.__init__(self, *args, **kwargs)
+        
+        self.setAttribute(Qt.WA_DeleteOnClose)
 
-        #loading image
-        self.pixmap = QPixmap('logo.jpg')
+        # loading image
+        self.pixmap = QPixmap("assets/logo.jpg")
         self.pixmap = self.pixmap.scaled(1000, 1000, Qt.KeepAspectRatio)
 
-        #creating label
+        # creating label
         self.label = QLabel(self)
         self.label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.label.setAlignment(Qt.AlignCenter)
 
-        #adding image to label
+        # adding image to label
         self.label.setPixmap(self.pixmap)
 
         self.layout = QGridLayout()
 
-        #adding label to screen
+        # adding label to screen
         self.layout.addWidget(self.label, 0, 0)
         self.setStyleSheet("background-color: black;")
 
         self.setLayout(self.layout)
         self.show()
 
-        #after 3 seconds, it hides the image label
-        def on_timeout():
-            self.label.hide()
+        # setting timer
+        QTimer.singleShot(3000, self.close_and_show_entry_screen)
+    
+    def close_and_show_entry_screen(self):
+        self.close()
+    
+    def closeEvent(self, event):
+        self.closed.emit()
+        event.accept()
 
-        #setting timer
-        QTimer.singleShot(3000, on_timeout)
+class PlayerEntryWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+    
+    def closeEvent(self, event):
+        # close here instead of after splash
+        sys.exit()
+        event.accept()
 
-app = QApplication(sys.argv)
-win = Window()
-win.showMaximized()
-sys.exit(app.exec_())
+def show_player_entry_screen():
+    global main_window
+    
+    main_window = PlayerEntryWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(main_window)
+    main_window.show()
+    
+    return main_window
+
+def show_splash_screen():
+    splashWin = SplashWindow()
+    splashWin.showMaximized()
+    return splashWin
+
+def main():
+    app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
+
+    splash_screen = show_splash_screen()
+    splash_screen.closed.connect(show_player_entry_screen)
+    
+    # don't close yet, we still need to open the main window
+    app.exec_()
+
+main()
